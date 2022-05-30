3d6324c0193ec52d7401832f5464b2182efcfd3d
Никита Глинкин <hermesshant@gmail.com>
2022-05-30 20:15:25 +0300

    add cors

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -4,6 +4,10 @@ const allowedCors = [
   'localhost:3000',
   'http://localhost:3000',
   'https://localhost:3000',
+  'https://movies.explorer.glinkin.nomoredomains.xyz',
+  'http://movies.explorer.glinkin.nomoredomains.xyz',
+  'https://movie.explorer.glinkin.nomoredomains.sbs',
+  'http://movie.explorer.glinkin.nomoredomains.sbs',
 ];
 
 module.exports = { allowedCors };
