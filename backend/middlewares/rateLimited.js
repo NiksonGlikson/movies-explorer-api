adea2415e5cce5fc9531cc8f382c3e1643347f1f
Никита Глинкин <hermesshant@gmail.com>
2022-08-07 18:48:24 +0300

    limit

--- backend/middlewares/rateLimited.js
+++ backend/middlewares/rateLimited.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 100000,
   delayMs: 0,
   messages: 'Вы сделали слишком много запросов за 15 минут',
   headers: true,
